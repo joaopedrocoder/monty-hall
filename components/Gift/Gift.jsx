db41a009d530f1b239c3617003d0cd236eea16e7
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-10-04 13:51:09 -0300

    desenvolvido o componente gift

---
+++ components/Gift/Gift.jsx
@@ -0,0 +1,14 @@
+import styles from './gift.module.css'
+
+const Gift = () => {
+    return (
+        <div className={styles.gift}>
+            <div className={styles.top}></div>
+            <div className={styles.body}></div>
+            <div className={styles.snood1}></div>
+            <div className={styles.snood2}></div>
+        </div>
+    )
+}
+
+export default Gift
